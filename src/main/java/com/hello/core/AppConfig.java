8a869c2dc572c827cd2a5655eebddaeb1cfbfd44
Lob77 <uueel1224@gmail.com>
2024-09-30 14:53:36 +0900

    [chore]AppConfig 중복제거

--- src/main/java/com/hello/core/AppConfig.java
+++ src/main/java/com/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.hello.core;
 
+import com.hello.core.discount.DiscoutPolicy;
 import com.hello.core.discount.FixDiscountPolicy;
+import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
 import com.hello.core.member.MemoryMemberRepository;
@@ -10,13 +12,21 @@ import com.hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discoutPolicy());
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    public DiscoutPolicy discoutPolicy (){
+        return new FixDiscountPolicy();
     }
 
 }
