d0b6c16c08ca590623e3a66dd4fd46ea7e66d7d8
Lob77 <uueel1224@gmail.com>
2024-09-30 15:08:21 +0900

    [refactor]스프링 코드로 변환

--- src/main/java/com/hello/core/MemberApp.java
+++ src/main/java/com/hello/core/MemberApp.java
@@ -5,12 +5,17 @@ import com.hello.core.member.Member;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
 import com.hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args){
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService =  appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService =  appConfig.memberService();
+        ApplicationContext applicationContext = new
+                AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
 
         Member member = new Member(1L,"memberA", Grade.VIP);
         memberService.join(member);
